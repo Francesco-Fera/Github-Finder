29ff97618c0a0108d341ed5fbe21149600ade18e
Francesco Fera <francescofera157@gmail.com>
2022-04-14 14:29:47 +0200

    First Commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-3xl">About Github Finder</h1>
+      <p>App to search Github users</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
+
+export default About
